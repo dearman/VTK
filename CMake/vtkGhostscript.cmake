3d1031d4b8157d42dd17674fa7f318b8c60cd0ea
Mark DeArman <mark@cascadeacoustic.com>
2018-12-29 02:44:57 -0800

    Fix documentation build.

--- CMake/vtkGhostscript.cmake
+++ CMake/vtkGhostscript.cmake
@@ -1,3 +1,3 @@
 # Find the GhostScript executable for GL2PS tests.
-find_program(VTK_GHOSTSCRIPT_EXECUTABLE gs gswin32c gsos2)
+find_program(VTK_GHOSTSCRIPT_EXECUTABLE mgs gs gswin32c gsos2)
 mark_as_advanced(VTK_GHOSTSCRIPT_EXECUTABLE)
